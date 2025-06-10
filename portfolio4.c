e006f69fabc9adf480bf35c5025e2e276dd78387
Mikaela Marcucci <mikaelamarcucci21@gmail.com>
2025-06-09 21:53:02 -0300

    atualizando

---
+++ portfolio4.c
@@ -0,0 +1,16 @@
+/* Crie um programa em linguagem C que:
+Cadastre até 5 alunos, utilizando uma struct com os seguintes campos:
+Nome (string)
+Idade (int)
+Nota final (float)
+Comandos adicionados via teclado (use fgets para o nome).
+Após o cadastro, o programa deve:
+Exibir todos os alunos cadastrados.
+Utilizar uma função para verificar e informar
+Qual a média da sala.
+Quantos alunos foram aprovados e reprovados.
+* Aprovado (nota ≥ 6.0)
+* Reprovado (nota < 6.0),
+Mostrar os alunos como reprovado apresentando nota e nome.
+*/
+
